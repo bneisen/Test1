539f871fc57d2cafcd54968c7b800b48f485a341
bneisen <greaserscc@gmail.com>
2015-02-05 16:04:19 -0600

    add angular and ember

---
+++ angularjs/cardsController.js
@@ -0,0 +1,9 @@
+﻿var myApp = angular.module('myApp', []);
+
+myApp.controller('cardsListController', ['$scope', function ($scope) {
+    $scope.cards = [{ id: 1 }, { id: 2 }];
+}]);
+
+myApp.controller('GreetingController', ['$scope', function ($scope) {
+    $scope.greeting = 'Hola!';
+}]);
